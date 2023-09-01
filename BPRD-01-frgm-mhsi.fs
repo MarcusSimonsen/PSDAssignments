60dbe17c0759e520c321cbbc6a82b9f3d1348718
frederikgantriis <frmo24042@gmail.com>
2023-09-01 11:57:52 +0200

    formatted code

--- BPRD-01-frgm-mhsi.fs
+++ BPRD-01-frgm-mhsi.fs
@@ -1,9 +1,3 @@
-(* Programming language concepts for software developers, 2010-08-28 *)
-
-(* Evaluating simple expressions with variables *)
-
-(* Association lists map object language variables to their values *)
-
 let env = [("a", 3); ("c", 78); ("baf", 666); ("b", 111)];;
 
 let emptyenv = []; (* the empty environment *)
@@ -24,15 +18,9 @@ type expr =
   | Prim of string * expr * expr
   | If of expr * expr * expr
 
-let e1 = CstI 17;;
-
-let e2 = Prim("+", CstI 3, Var "a");;
-
-let e3 = Prim("+", Prim("*", Var "b", CstI 9), Var "a");;
-
 
 (* Evaluation within an environment *)
-let rec eval e (env : (string * int) list) : int =
+let rec eval e (env : (string * int) list) : int = //Edited
     match e with
     | CstI i            -> i
     | Var x             -> lookup env x 
@@ -46,7 +34,7 @@ let rec eval e (env : (string * int) list) : int =
     | Prim _            -> failwith "unknown primitive"
 
 
-let rec eval2 e (env : (string * int) list) : int =
+let rec eval2 e (env : (string * int) list) : int = //Created
     match e with
     | CstI i            -> i
     | Var x             -> lookup env x 
@@ -61,11 +49,8 @@ let rec eval2 e (env : (string * int) list) : int =
         | "max" -> (if i1 > i2 then i1 else i2)
         | "==" -> (if i1 = i2 then 1 else 0)
         | _   -> failwith "unknown primitive"
-
-let e1v  = eval e1 env;;
-let e2v1 = eval e2 env;;
-let e2v2 = eval e2 [("a", 314)];;
-let e3v  = eval e3 env;;
+    | _   -> failwith "unknown type"
+    
 
 let eMin1 = Prim("min", CstI 3, CstI 4);;
 let eMin2 = Prim("min", CstI 4, CstI 3);;
@@ -77,14 +62,8 @@ let eEq1 = Prim("==", CstI 3, CstI 4);;
 let eEq2 = Prim("==", CstI 4, CstI 4);;
 let eEq3 = Prim("==", Prim("+", CstI 2, CstI 2), CstI 3);;
 
-
+(* Test Cases for eval for min, max, equal *)
 let printInt h = printfn "%A" h
-
-printInt e1v
-printInt e2v1
-printInt e2v2
-printInt e3v
-
 let eMin1v = eval eMin1 env;;
 let eMin2v = eval eMin2 env;;
 let eMin3v = eval eMin3 env;;
@@ -95,9 +74,8 @@ let eEq1v = eval eEq1 env;;
 let eEq2v = eval eEq2 env;;
 let eEq3v = eval eEq3 env;;
 
-let eIf1 = If(Var "a", CstI 11, CstI 22);;
 
-let eIf1v = eval eIf1 env;;
+printfn "Test cases for exercise 1.1 i - iii"
 
 printInt eMin1v
 printInt eMin2v
@@ -109,7 +87,15 @@ printInt eEq1v
 printInt eEq2v
 printInt eEq3v
 
+(* Test cases for If-then-else *)
+
+printfn "Test cases for exercise 1.1 iv and v"
+let eIf1 = If(Var "a", CstI 11, CstI 22);;
+let eIf1v = eval eIf1 env;;
 printInt eIf1v
+
+
+(* Arithmetic Expressions *)
 
 type aexpr =
     | CstI of int
@@ -121,7 +107,6 @@ type aexpr =
 let aexpr1 = Sub(Var "v", Add(Var "w", Var "z"))
 let aexpr2 = Mul(CstI 2, Sub(Var "v", Add(Var "w", Var "z")))
 let aexpr3 = Add(Add(Add(Var "x", Var "y"), Var "z"), Var "v")
-
 let rec fmt : aexpr -> string = function
     | CstI i -> sprintf "%d" i
     | Var x -> x
@@ -133,10 +118,13 @@ let aexpr1s = fmt aexpr1
 let aexpr2s = fmt aexpr2
 let aexpr3s = fmt aexpr3
 
+printfn "Test cases for exercise 1.2 i - iii"
+
 printfn "%s" aexpr1s
 printfn "%s" aexpr2s
 printfn "%s" aexpr3s
 
+(* Simplification of arithmetic expressions *)
 let rec simplify: aexpr -> aexpr = function
     | CstI i -> CstI i
     | Var x -> Var x
@@ -161,7 +149,7 @@ let rec simplify: aexpr -> aexpr = function
         let e2v = simplify e2
         match e1v, e2v with
         | CstI x, CstI y when x = 0 || y = 0 -> CstI 0
-        | Var x, CstI 0 | CstI 0, Var x -> CstI 0
+        | Var _, CstI 0 | CstI 0, Var _ -> CstI 0
         | CstI x, CstI y when x = 1 -> CstI y
         | CstI x, CstI y when y = 1 -> CstI x
         | Var x, CstI 1 | CstI 1, Var x -> Var x
@@ -176,12 +164,15 @@ let aexprTest1s = simplify aexprTest1
 let aexprTest2s = simplify aexprTest2
 let aexprTest3s = simplify aexprTest3
 
+printfn "Test cases for exercise 1.2 iv"
+
 printfn "%A" aexprTest1s
 
 printfn "%A" aexprTest2s
 
 printfn "%A" aexprTest3s
 
+(* Symbolic differentiation of arithmetic expressions *)
 let rec symbDiff (e:aexpr) x : aexpr =
     match e with
     | CstI _ -> CstI 0
@@ -204,6 +195,8 @@ let diffTest10 = symbDiff (Mul(Var "x", Var "y")) "x" // y
 let diffTest11 = symbDiff (Mul(Var "x", Var "y")) "y" // x
 let diffTest12 = symbDiff (Mul(Var "x", Var "x")) "x" // 2x
 
+printfn "Test cases for exercise 1.2 v"
+
 printfn "%A should be 0" (diffTest1 |> simplify)
 printfn "%A should be 1" (diffTest2 |> simplify)
 printfn "%A should be 0" (diffTest3 |> simplify)
@@ -216,3 +209,97 @@ printfn "%A should be 0" (diffTest9 |> simplify)
 printfn "%A should be y" (diffTest10 |> simplify)
 printfn "%A should be x" (diffTest11 |> simplify)
 printfn "%A should be 2x" (diffTest12 |> simplify)
+
+
+
+(* Part Two *)
+
+type expr2 = 
+  | CstI of int
+  | Var of string
+  | Let of (string * expr2) list * expr2
+  | Prim of string * expr2 * expr2;;
+
+
+let rec eval3 e (env : (string * int) list) : int =
+    match e with
+    | CstI i            -> i
+    | Var x             -> lookup env x 
+    | Let(xs, ebody) -> eval3 ebody (List.fold (fun acc (x, y) -> (x, eval3 y acc) :: acc) env xs)
+    | Prim("+", e1, e2) -> eval3 e1 env + eval3 e2 env
+    | Prim("*", e1, e2) -> eval3 e1 env * eval3 e2 env
+    | Prim("-", e1, e2) -> eval3 e1 env - eval3 e2 env
+    | Prim _            -> failwith "unknown primitive";;
+
+let run e = eval3 e [];;
+
+printfn "Test cases for exercise 2.1"
+let exp1 = Let([("x1", Prim("+", CstI 5, CstI 7)); ("x2", Prim("*", Var "x1", CstI 2))], Prim("+", Var "x1", Var "x2"))
+
+let res2 = List.map run [exp1]
+
+List.fold (fun acc x -> printfn "%A" x |> ignore; acc) res2 res2
+
+let rec mem x vs = 
+    match vs with
+    | []      -> false
+    | v :: vr -> x=v || mem x vr;;
+
+let rec union (xs, ys) = 
+    match xs with 
+    | []    -> ys
+    | x::xr -> if mem x ys then union(xr, ys)
+               else x :: union(xr, ys);;
+
+let rec minus (xs, ys) = 
+    match xs with 
+    | []    -> []
+    | x::xr -> if mem x ys then minus(xr, ys)
+               else x :: minus (xr, ys);;
+
+let rec freevars e : string list =
+    match e with
+    | CstI _ -> []
+    | Var x  -> [x]
+    | Let(xs, ebody) -> List.fold (fun _ (x, y) -> union (freevars y, minus (freevars ebody, [x]))) [] xs 
+    | Prim(_, e1, e2) -> union (freevars e1, freevars e2);;
+
+printfn "Test cases for exercise 2.2"
+
+printfn "%A" (freevars (Let([("x1", Prim("+", Var "x1", CstI 7))], Prim("+", Var "x1", CstI 8))))
+
+type texpr =                            (* target expressions *)
+  | TCstI of int
+  | TVar of int                         (* index into runtime environment *)
+  | TLet of texpr * texpr               (* erhs and ebody                 *)
+  | TPrim of string * texpr * texpr;;
+
+let rec getindex vs x = 
+    match vs with 
+    | []    -> failwith "Variable not found"
+    | y::yr -> if x=y then 0 else 1 + getindex yr x;;
+
+(* Compiling from expr to texpr *)
+
+let rec tcomp (e : expr2) (cenv : string list) : texpr =
+    match e with
+    | CstI i -> TCstI i
+    | Var x  -> TVar (getindex cenv x)
+    | Let(xs, ebody) -> 
+        let cenv1 = List.fold (fun (acc1,acc2)  (x, y) -> (x :: acc1, y :: acc2)) (cenv, []) xs
+        let exp = Let(xs, ebody)
+        TLet(tcomp exp cenv, tcomp ebody (fst cenv1))
+    | Prim(ope, e1, e2) -> TPrim(ope, tcomp e1 cenv, tcomp e2 cenv);;
+
+let rec teval (e : texpr) (renv : int list) : int =
+    match e with
+    | TCstI i -> i
+    | TVar n  -> List.item n renv
+    | TLet(erhs, ebody) -> 
+      let xval = teval erhs renv
+      let renv1 = xval :: renv 
+      teval ebody renv1 
+    | TPrim("+", e1, e2) -> teval e1 renv + teval e2 renv
+    | TPrim("*", e1, e2) -> teval e1 renv * teval e2 renv
+    | TPrim("-", e1, e2) -> teval e1 renv - teval e2 renv
+    | TPrim _            -> failwith "unknown primitive";;
