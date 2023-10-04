0ad768c9dfb912bcdf2227ccdeefc68229ff2601
Marcus Henrik Simonsen <mhsi@itu.dk>
2023-10-04 16:45:34 +0200

    Add alternative solution to 5.1

--- Assignment05/Assignment05.fs
+++ Assignment05/Assignment05.fs
@@ -4,3 +4,21 @@ let rec merge (xs: int list) (ys: int list) : int list =
     | [], ys -> ys
     | xs, [] -> xs
     | x::xs, y::ys -> if x < y then x::(merge xs (y::ys)) else y::(merge (x::xs) ys)
+
+/* Version that doesn't overflow the stack */
+let mergeAux (xs: int list) (ys: int list) : int list =
+    let rec aux xs ys acc =
+        match xs, ys with
+        | [], [] -> acc
+        | [], ys -> ys @ acc
+        | xs, [] -> xs @ acc
+        | x::xs, y::ys -> if x < y then aux xs (y::ys) (x::acc) else aux (x::xs) ys (y::acc)
+    aux xs ys []
+
+/* Test code */
+/*
+let a = [0..1000000]
+let b = [0..1000000]
+
+let c = merge a b
+*/
