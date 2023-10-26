608f2753b0ccdb972f283ec6b674882896829202
Marcus Henrik Simonsen <mhsi@itu.dk>
2023-10-26 09:25:05 +0200

    Make test7-2.sh not platform dependant

--- Assignment06/MicroC/test7-2.sh
+++ Assignment06/MicroC/test7-2.sh
@@ -1,1 +1,1 @@
-echo "open ParseAndRun;; run (fromFile \"7-2.c\") [];;" | dotnet fsi -r ~/bin/fsharp/FsLexYacc.Runtime.dll Absyn.fs CPar.fs CLex.fs Parse.fs Interp.fs ParseAndRun.fs
+echo "open ParseAndRun;; run (fromFile \"7-2.c\") [];;" | dotnet fsi -r FsLexYacc.Runtime.dll Absyn.fs CPar.fs CLex.fs Parse.fs Interp.fs ParseAndRun.fs
