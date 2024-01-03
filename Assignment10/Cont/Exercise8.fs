42189750705ca7d485f549f6ab9358e94c076f8a
Marcus Henrik Simonsen <mhsi@itu.dk>
2024-01-03 13:02:46 +0100

    Remove comment

--- Assignment10/Cont/Exercise8.fs
+++ Assignment10/Cont/Exercise8.fs
@@ -12,7 +12,5 @@ run (Every(Write(Prim("+", Prim("*", CstI 10, FromTo(2, 4)), FromTo(1, 2)))))
 (* Exercise 11.8 (ii) *)
 printf "Exercise 8 (ii)\n"
 
-(* Write an expression that prints the least multiple of 7 that is greater than 50. *)
-
 run (Write(Prim("<", CstI 50, Prim("*", CstI 7, FromTo(1, 1000)))))
 
